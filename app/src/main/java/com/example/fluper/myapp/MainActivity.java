bfa13d9388ed32d4e75c509e5171837554d8b6fa
Prabhat Singh <prbt1996@gmail.com>
2018-04-03 19:55:54 +0530

    MyApp

--- app/src/main/java/com/example/fluper/myapp/MainActivity.java
+++ app/src/main/java/com/example/fluper/myapp/MainActivity.java
@@ -7,10 +7,12 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -78,6 +80,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+
+
+
+
 
         adapter = new DbAdapter(this);
 
--- app/src/main/java/com/example/fluper/myapp/appUtil/BaseActivity.java
+++ app/src/main/java/com/example/fluper/myapp/appUtil/BaseActivity.java
@@ -6,12 +6,18 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 
 import com.example.fluper.myapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -21,12 +27,58 @@ public class BaseActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private GridView gv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
         navigationDrawer();
+
+        SearchView searchView = findViewById (R.id.notification_icon);
+        gv = findViewById (R.id.mGridView);
+
+        List<String> galaxies=new ArrayList<> ();
+        galaxies.add("Cartwheel");
+        galaxies.add("Whirlpool");
+        galaxies.add("Andromeda I");
+        galaxies.add("Andromeda II");
+        galaxies.add("Sombrero");
+        galaxies.add("Messier 81");
+        galaxies.add("Messier 87");
+        galaxies.add("Canis Majos Over-density");
+        galaxies.add("Messier 92");
+        galaxies.add("Black Eye");
+        galaxies.add("Centaurus A");
+        galaxies.add("Centaurus B");
+        galaxies.add("Milky Way");
+        galaxies.add("IC 1011");
+        galaxies.add("Star Bust");
+        galaxies.add("Hoag's Object");
+        galaxies.add("Pinwheel");
+        galaxies.add("Triangulum");
+        galaxies.add("Cosmos Redshift");
+        galaxies.add("Ursa Minor");
+        galaxies.add("Virgo Stellar-Stream");
+
+        //ADAPTER
+        final ArrayAdapter adapter=new ArrayAdapter<String> (this,android.R.layout.simple_list_item_1,galaxies);
+        gv.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener (new SearchView.OnQueryTextListener () {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                gv.setVisibility (View.VISIBLE);
+                return false;
+            }
+        });
+
     }
 
 
@@ -38,8 +90,8 @@ public class BaseActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.include_tool_layout);
         humburger = toolbar.findViewById(R.id.side_bar_icon);
-        edit = toolbar.findViewById(R.id.notification_icon);
-        edit.setVisibility(View.GONE);
+     //   edit = toolbar.findViewById(R.id.notification_icon);
+      //  edit.setVisibility(View.GONE);
 
         ActionBarDrawerToggle abdt = new ActionBarDrawerToggle(this, dl, toolbar, R.string.open, R.string.close);
         dl.addDrawerListener(abdt);
@@ -54,5 +106,7 @@ public class BaseActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
 }
